8dc65ebe15802ae327690fc6b0fcdcea64bf5105
Alex Angerer <alex.angerer@schreiner48.ch>
2024-04-21 21:44:13 +0200

    Hangman vor Debuging

--- Projekt Hangman/Program.cs
+++ Projekt Hangman/Program.cs
@@ -28,7 +28,7 @@ class Program
             switch (action)
             {
                 case 1:
-                    // StartGame();
+                    StartGame();
                     break;
                 case 2:
                     end = true;
@@ -61,6 +61,100 @@ class Program
         Random rnd = new Random();
         int index = rnd.Next(0, words.Length);
         string word = words[index].ToLower();
-        // GameLoop();
+        GameLoop(word);
+    }
+
+    static void GameLoop(string word)
+    {
+        int lives = 10;
+        string hiddenWord = "";
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            hiddenWord += "_";
+        }
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Gesuchtes Wort: {0}", hiddenWord);
+            Console.Write("Noch übrige Versuche: ");
+
+            for (int i = 0; i < lives; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write('X');
+                Console.ResetColor();
+            }
+
+            Console.WriteLine();
+            Console.Write("Buchstabe: ");
+            char character = Convert.ToChar(Console.ReadLine().ToLower());
+
+            bool foundCharacterInWord = false;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == character)
+                {
+                    foundCharacterInWord = true;
+                    break;
+                }
+            }
+
+            string tempHiddenWord = hiddenWord;
+            hiddenWord = "";
+
+            if (foundCharacterInWord)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == character)
+                    {
+                        hiddenWord += character;
+                    }
+                    else if (tempHiddenWord[i] != '_')
+                    {
+                        hiddenWord += tempHiddenWord[i];
+                    }
+                    else
+                    {
+                        hiddenWord += '_';
+                    }
+                }
+
+                if (hiddenWord == word)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("GEWONNEN!!!");
+                    Console.WriteLine("Das Wort war: {0}", word);
+                    Console.ReadKey();
+                    Console.ResetColor();
+                    break;
+                }
+                else
+                {
+                    hiddenWord = tempHiddenWord;
+
+                    if (lives > 0)
+                    {
+                        lives -= 1;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("GAME OVER!");
+                        Console.ReadKey();
+                        Console.ResetColor();
+                        break;
+                    }
+                }
+            }
+            {
+                
+            }
+        }
     }
 }
