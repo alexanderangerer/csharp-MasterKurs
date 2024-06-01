ab32a6e808be847dacac4e5ee63d01a871a45d81
alexanderangerer <angerer.alexander@gmail.com>
2024-06-01 15:43:21 +0200

    OOP weiter gemacht

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,8 +1,28 @@
-﻿namespace OOP;
+﻿using System.Runtime.CompilerServices;
+
+namespace OOP;
 
 class Program
 {
     static void Main(string[] args)
+    {
+        // KlassenInitialisieren();
+        Vererbung();
+    }
+
+    static void Vererbung()
+    {
+        Notebook mba = new Notebook("M1", 64);
+        // mba.Prozessor = "M1";
+        // mba.Arbeitspeicher = 64;
+        // mba.Batteriestand = 55;
+        
+        mba.Einschalten();
+        mba.Laden();
+        mba.Ausschalten();
+    }
+
+    static void KlassenInitialisieren()
     {
         Bankkonto konto = new Bankkonto();
 
@@ -16,5 +36,40 @@ class Program
         Console.WriteLine(konto.Bankname);
         konto.Bankname = "";
         Console.WriteLine(konto.Bankname);
+    }
+}
+class Notebook : Computer
+{
+    public Notebook(string prozessor, int ram) : base(prozessor, ram)
+    {
+        Console.WriteLine("Konstruktor in der Notebook-Klasse.");
+        Batteriestand = 100;
+    }
+    public int Batteriestand { get; set; }
+
+    public void Laden()
+    {
+        Console.WriteLine("Notebook: Computer wird geladen.");
+    }
+}
+class Computer
+{
+    public Computer(string prozessor, int ram)
+    {
+        Console.WriteLine("Konstruktor in der Computer-Klasse.");
+        Prozessor = prozessor;
+        Arbeitspeicher = ram;
+    }
+    public string Prozessor { get; set; }
+    public int Arbeitspeicher { get; set; }
+
+    public void Einschalten()
+    {
+        Console.WriteLine("Computer: Der Computer wurde eingeschaltet.");
+    }
+
+    public void Ausschalten()
+    {
+        Console.WriteLine("Computer: Der Computer wird ausgeschaltet.");
     }
 }
