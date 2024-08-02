3d579220e70467fcbd44933f8728fc45f8376d7e
Alex Angerer <alex.angerer@schreiner48.ch>
2024-08-02 07:05:21 +0200

    Neues Kapitel: Collections

---
+++ Collections/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<string> ortStack = new Stack<string>();
+        
+        ortStack.Push("Safenwil");
+        ortStack.Push("Kloten");
+        ortStack.Push("Winden");
+
+        Console.WriteLine(ortStack.Count());
+
+        Console.WriteLine(ortStack.Pop());
+        Console.WriteLine(ortStack.Peek());
+        Console.WriteLine(ortStack.Pop());
+        Console.WriteLine(ortStack.Peek());
+        
+        // List<string> namensList = new List<string>();
+        // List<string> ortList = new List<string>()
+        // {
+        //     "Norddeich",
+        //     "Norden",
+        //     "Winden",
+        //     "Kloten",
+        //     "Safenwil"
+        // };
+        //
+        // namensList.Add("Kerstin");
+        // namensList.Add("Lenia");
+        // namensList.Add("Malia");
+        // namensList.Add("Alex");
+        //
+        // Console.WriteLine(namensList[2]);
+        // Console.WriteLine(ortList[2]);
+        //
+        // Console.WriteLine(namensList.First());
+        // Console.WriteLine(ortList.Last());
+        //
+        // namensList[3] = "Angerer";
+        // Console.WriteLine(namensList[3]);
+        // namensList.Remove("Angerer");
+        // Console.WriteLine(namensList.Count);
+        //
+        // Console.WriteLine(ortList.Count);
+    }
+}
